726c01d2615056ea49f9ba28fd4c863677e70ff9
tfermoso <tomas.fermoso@gmail.com>
2024-12-26 19:22:09 +0100

    mostrando mensaje

--- js/registro.js
+++ js/registro.js
@@ -1,6 +1,7 @@
 var inputPass=document.getElementById("password");
 var inputRePass=document.getElementById("repassword");
 var btnCrear=document.getElementById("btnCrear");
+var msg=document.getElementById("msg");
 
 inputPass.oninput=validacionInput;
 inputRePass.oninput=validacionInput;
@@ -10,8 +11,10 @@ function validacionInput(){
     var txtRePass=inputRePass.value;
     if(txtPass==txtRePass){
         btnCrear.disabled=false;
+        msg.style.display="none";
     }else{
         btnCrear.disabled=true;
+        msg.style.display="block";
     }
     //console.log(txtPass);
 }
