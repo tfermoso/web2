68d4c86a05b8eb5c591d52fcb07bfc687fe452bd
tfermoso <tomas.fermoso@gmail.com>
2024-12-27 16:48:58 +0100

    funcion de habilitar btnEnviar

--- js/contacto.js
+++ js/contacto.js
@@ -1,9 +1,9 @@
 var checkAuth=document.getElementById("autorizacion");
-
+var btnEnviar=document.getElementById("btnEnviar");
 checkAuth.onclick=function(){
     if(checkAuth.checked==true){
-
+        btnEnviar.disabled=false;
     }else{
-        
+        btnEnviar.disabled=true;
     }
 }
