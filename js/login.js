6b34765952ad543d9a720f263665d8c6814dcff2
tfermoso <tomas.fermoso@gmail.com>
2024-12-27 18:17:15 +0100

    arreglando estilos

---
+++ js/login.js
@@ -0,0 +1,11 @@
+var formulario = document.getElementById('formulario');
+formulario.onsubmit = function (e) {
+    e.preventDefault();
+    var usuario = document.getElementById('usuario').value;
+    var password = document.getElementById('password').value;
+    if (usuario == 'admin' && password == 'admin') {
+        window.location.href = 'main.html';
+    }else{
+        alert('Usuario o contraseña incorrectos');
+    }
+}
