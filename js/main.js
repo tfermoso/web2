07075d472e74b515884d2f77a82c5ec2a80efd02
tfermoso <tomas.fermoso@gmail.com>
2025-01-02 17:05:51 +0100

    mostrando menu

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var user=document.getElementById("imgUser");
+user.onclick=function(){
+    var menuUser=document.getElementById("menuUser");
+    if(menuUser.style.display=="block"){
+        menuUser.style.display="none";
+    }else{
+        menuUser.style.display="block";
+    }
+};
