044ab2f4e8bf4f675857167d64360055b6478440
tfermoso <tomas.fermoso@gmail.com>
2025-01-15 18:43:23 +0100

    editar incidencia en bbdd

--- editar_incidencia.php
+++ editar_incidencia.php
@@ -3,31 +3,42 @@ session_start();
 if (!isset($_SESSION["usuario"])) {
     header("Location: login.php");
 }
-if(! isset($_GET["idincidencia"])) {
+if (!isset($_GET["idincidencia"])) {
     header("Location: main.php");
 }
 include("conexiondb.php");
-$sql="select * from incidencias where id = :idincidencia";
-$stm=$conexion->prepare($sql);
-$stm->bindParam(":idincidencia",$_GET["idincidencia"]);
-$stm->execute();
-$row=$stm->fetch(PDO::FETCH_ASSOC);
-if(! $row){
-    header("Location: main.php");
+if (isset($_POST["id"])) {
+//Editar incidencia en bbdd
+    $sql = "UPDATE incidencias SET fecha = :fecha, descripcion = :descripcion WHERE id = :id";
+    $stm = $conexion->prepare($sql);
+    $stm->bindParam(":fecha", $_POST["fecha"]);
+    $stm->bindParam(":descripcion", $_POST["descripcion"]);
+    $stm->bindParam(":id", $_POST["id"]);
+    $stm->execute();
+    header("Location: main.php"); 
+} else {
+    $sql = "select * from incidencias where id = :idincidencia";
+    $stm = $conexion->prepare($sql);
+    $stm->bindParam(":idincidencia", $_GET["idincidencia"]);
+    $stm->execute();
+    $row = $stm->fetch(PDO::FETCH_ASSOC);
+    if (!$row) {
+        header("Location: main.php");
+    }
+    include("./partials/cabecera.php");
 }
-include("./partials/cabecera.php");
 ?>
 <div class="contenedorPrincipal">
-<form action="" method="post">
-    <label for="id">Id Incidencia</label>
-    <input type="number" name="id" id="" readonly value="<?php echo $row['id'] ?>">
-    <label for="fecha">Fecha</label>
-    <input type="date" name="fecha" id="fecha" value="<?php echo $row['fecha'] ?>">
-    <label for="descripcion">Descripcion</label>   
-    <input type="text" name="descripcion" id="descripcion" value="<?php echo $row['descripcion'] ?>">
-    <input type="submit" value="Editar">
-</form>
+    <form action="" method="post">
+        <label for="id">Id Incidencia</label>
+        <input type="number" name="id" id="" readonly value="<?php echo $row['id'] ?>">
+        <label for="fecha">Fecha</label>
+        <input type="date" name="fecha" id="fecha" value="<?php echo $row['fecha'] ?>">
+        <label for="descripcion">Descripcion</label>
+        <input type="text" name="descripcion" id="descripcion" value="<?php echo $row['descripcion'] ?>">
+        <input type="submit" value="Editar">
+    </form>
 </div>
 <?php
-include("./partials/footer.php");   
+include("./partials/footer.php");
 ?>
