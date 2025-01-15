94c5e7299e891826ecd01efe2f2e306d967ae4ea
tfermoso <tomas.fermoso@gmail.com>
2025-01-15 18:16:20 +0100

    cargando incidencia a editar

--- editar_incidencia.php
+++ editar_incidencia.php
@@ -3,10 +3,23 @@ session_start();
 if (!isset($_SESSION["usuario"])) {
     header("Location: login.php");
 }
+if(! isset($_GET["idincidencia"])) {
+    header("Location: main.php");
+}
+include("conexiondb.php");
+$sql="select * from incidencias where id = :idincidencia";
+$stm=$conexion->prepare($sql);
+$stm->bindParam(":idincidencia",$_GET["idincidencia"]);
+$stm->execute();
+$row=$stm->fetch(PDO::FETCH_ASSOC);
+if(! $row){
+    header("Location: main.php");
+}
+
 include("./partials/cabecera.php");
 ?>
 <div class="contenedorPrincipal">
-    
+
 </div>
 <?php
 include("./partials/footer.php");   
