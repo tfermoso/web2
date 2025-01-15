103382afd0e191fdaa37b28d2744865be1c2439a
tfermoso <tomas.fermoso@gmail.com>
2025-01-15 18:08:21 +0100

    cabecera y footer

--- editar_incidencia.php
+++ editar_incidencia.php
@@ -1,7 +1,12 @@
 <?php
+session_start();
+if (!isset($_SESSION["usuario"])) {
+    header("Location: login.php");
+}
 include("./partials/cabecera.php");
 ?>
 <div class="contenedorPrincipal">
+    
 </div>
 <?php
 include("./partials/footer.php");   
