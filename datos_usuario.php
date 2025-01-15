c89daf83dc70d16886c86ef0354b02016b8ee1aa
tfermoso <tomas.fermoso@gmail.com>
2025-01-15 19:23:01 +0100

    formulario editar usuario

--- datos_usuario.php
+++ datos_usuario.php
@@ -4,7 +4,7 @@ if (!isset($_SESSION["usuario"])) {
     header("Location: login.php");
 }
 include("conexiondb.php");
-$sql="select * from usuarios where idusuario = :idusuario";
+$sql="select * from usuarios where id = :idusuario";
 $stm=$conexion->prepare($sql);
 $stm->bindParam(":idusuario",$_SESSION["idusuario"]);
 $stm->execute();
@@ -15,7 +15,17 @@ if(!$row){
 include("./partials/cabecera.php");
 ?>
 <div class="contenedorPrincipal">
-    
+    <form action="" method="post">
+        <label for="id">Id Usuario</label>
+        <input type="number" name="id" id="" readonly value="<?php echo $row['id'] ?>">
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre" id="nombre" value="<?php echo $row['nombre'] ?>">
+        <label for="apellidos">Apellidos</label>
+        <input type="text" name="apellidos" id="apellidos" value="<?php echo $row['apellidos'] ?>">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" value="<?php echo $row['email'] ?>">
+        <input type="submit" value="Editar">
+    </form>
 </div>
 <?php
 include("./partials/footer.php");
